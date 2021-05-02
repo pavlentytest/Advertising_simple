6bddd0f46c66598c8fbd2a6b296b112c58fd3024
HelenDruzhinskaya <alena1806@mail.ru>
2021-05-02 15:28:44 +0500

    Initial

---
+++ app/src/main/java/ru/myitacademy/samsung/advertising/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.myitacademy.samsung.advertising
+
+import android.Manifest
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+var myIntent = Intent("ru.myitacademy.samsung.advertising.POST")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.allbtn).setOnClickListener {
+            myIntent.putExtra("hot_news", findViewById<EditText>(R.id.advtxt).text.toString())
+            //sendBroadcast(intent)
+            sendOrderedBroadcast(myIntent,null)}
+    }
+fun selectPost(view: View){
+    sendOrderedBroadcast(myIntent,Manifest.permission.INTERNET)
+}
+
+}
