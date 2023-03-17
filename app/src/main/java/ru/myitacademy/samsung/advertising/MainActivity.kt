e0c0f9787abbb191f0be3d56da767b549cf67e5f
Pavel <impetus@yandex.ru>
2023-03-17 18:42:56 +0300

    Simplify

--- app/src/main/java/ru/myitacademy/samsung/advertising/MainActivity.kt
+++ app/src/main/java/ru/myitacademy/samsung/advertising/MainActivity.kt
@@ -9,21 +9,15 @@ import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-var myIntent = Intent("ru.myitacademy.samsung.advertising.POST")
+    var myIntent = Intent("ru.myitacademy.samsung.advertising.POST")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.allbtn).setOnClickListener {
             myIntent.putExtra("hot_news", findViewById<EditText>(R.id.advtxt).text.toString())
-            //sendBroadcast(intent)
+
             sendOrderedBroadcast(myIntent,null)}
     }
-fun selectPost(view: View){
-    sendOrderedBroadcast(myIntent,Manifest.permission.INTERNET)
-}
- fun backPost(view:View){
-     sendOrderedBroadcast(myIntent,null,
-                          MyReceiver(),null,0,"NewPost",
-           Bundle().apply{putString("strEx",findViewById<EditText>(R.id.advtxt).text.toString())})
- }
+
+
 }
